a4918d7b1b7fec0e549195075eee45a20c9a8f06
Simon Larsen <slarse@kth.se>
2017-08-04 13:28:39 +0200

    Bump PATCH version number

--- setup.py
+++ setup.py
@@ -13,13 +13,13 @@ required = ['daiquiri']
 
 setup(
     name='clanim',
-    version='0.1.0',
+    version='0.1.3',
     description=('Function decorators that cause a command line animation to be run for the duration of the function.'),
     long_description=readme,
     author='Simon Larsén',
     author_email='slarse@kth.se',
     url='https://github.com/slarse/clanim',
-    download_url='https://github.com/slarse/clanim/archive/v0.1.0.tar.gz',
+    download_url='https://github.com/slarse/clanim/archive/v0.1.3.tar.gz',
     license=license,
     packages=find_packages(exclude=('tests', 'docs')),
     tests_require=test_requirements,
