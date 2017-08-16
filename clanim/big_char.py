16445e5485a8c058f757dcf9f26e4148f29fbd4f
Simon Larsen <slarse@kth.se>
2017-08-16 19:32:49 +0200

    Prepend underscore to all characters in big_char module

--- clanim/big_char.py
+++ clanim/big_char.py
@@ -10,184 +10,184 @@
 CHAR_HEIGHT = 5
 CHAR_WIDTH = 5
 
-A = [' XXX ',
-     'X   X',
-     'XXXXX',
-     'X   X',
-     'X   X']
+_A = [' XXX ',
+      'X   X',
+      'XXXXX',
+      'X   X',
+      'X   X']
 
-B = ['XXXX ',
-     'X   X',
-     'XXXXX',
-     'X   X',
-     'XXXX ']
+_B = ['XXXX ',
+      'X   X',
+      'XXXXX',
+      'X   X',
+      'XXXX ']
 
-C = ['XXXXX',
-     'X    ',
-     'X    ',
-     'X    ',
-     'XXXXX']
+_C = ['XXXXX',
+      'X    ',
+      'X    ',
+      'X    ',
+      'XXXXX']
 
-D = ['XXXX ',
-     'X   X',
-     'X   X',
-     'X   X',
-     'XXXX ']
+_D = ['XXXX ',
+      'X   X',
+      'X   X',
+      'X   X',
+      'XXXX ']
 
-E = ['XXXXX',
-     'X    ',
-     'XXXXX',
-     'X    ',
-     'XXXXX']
+_E = ['XXXXX',
+      'X    ',
+      'XXXXX',
+      'X    ',
+      'XXXXX']
 
-F = ['XXXXX',
-     'X    ',
-     'XXXXX',
-     'X    ',
-     'X    ']
+_F = ['XXXXX',
+      'X    ',
+      'XXXXX',
+      'X    ',
+      'X    ']
 
-G = ['XXXXX',
-     'X    ',
-     'X  XX',
-     'X   X',
-     'XXXXX']
+_G = ['XXXXX',
+      'X    ',
+      'X  XX',
+      'X   X',
+      'XXXXX']
 
-H = ['X   X',
-     'X   X',
-     'XXXXX',
-     'X   X',
-     'X   X']
+_H = ['X   X',
+      'X   X',
+      'XXXXX',
+      'X   X',
+      'X   X']
 
-I = ['XXXXX',
-     '  X  ',
-     '  X  ',
-     '  X  ',
-     'XXXXX']
+_I = ['XXXXX',
+      '  X  ',
+      '  X  ',
+      '  X  ',
+      'XXXXX']
 
-J = ['XXXXX',
-     '    X',
-     '    X',
-     'x   X',
-     'XXXXX']
+_J = ['XXXXX',
+      '    X',
+      '    X',
+      'x   X',
+      'XXXXX']
 
-K = ['X   X',
-     'X  X ',
-     'XXX  ',
-     'X  X ',
-     'X   X']
+_K = ['X   X',
+      'X  X ',
+      'XXX  ',
+      'X  X ',
+      'X   X']
 
-L = ['X    ',
-     'X    ',
-     'X    ',
-     'X    ',
-     'XXXXX']
+_L = ['X    ',
+      'X    ',
+      'X    ',
+      'X    ',
+      'XXXXX']
 
-M = [' X X ',
-     'X X X',
-     'X X X',
-     'X   X',
-     'X   X']
+_M = [' X X ',
+      'X X X',
+      'X X X',
+      'X   X',
+      'X   X']
 
-N = ['X   X',
-     'XX  X',
-     'X X X',
-     'X  XX',
-     'X   X']
+_N = ['X   X',
+      'XX  X',
+      'X X X',
+      'X  XX',
+      'X   X']
 
-O = [' XXX ',
-     'X   X',
-     'X   X',
-     'X   X',
-     ' XXX ']
+_O = [' XXX ',
+      'X   X',
+      'X   X',
+      'X   X',
+      ' XXX ']
 
-P = ['XXXX ',
-     'X   X',
-     'XXXX ',
-     'X    ',
-     'X    ']
+_P = ['XXXX ',
+      'X   X',
+      'XXXX ',
+      'X    ',
+      'X    ']
 
-Q = [' XXX ',
-     'X   X',
-     'X   X',
-     ' XXX ',
-     'XX   ']
+_Q = [' XXX ',
+      'X   X',
+      'X   X',
+      ' XXX ',
+      'XX   ']
 
-R = ['XXXX ',
-     'X   X',
-     'XXXX ',
-     'X   X',
-     'X   X']
+_R = ['XXXX ',
+      'X   X',
+      'XXXX ',
+      'X   X',
+      'X   X']
 
-S = ['XXXXX',
-     'X    ',
-     'XXXXX',
-     '    X',
-     'XXXXX']
+_S = ['XXXXX',
+      'X    ',
+      'XXXXX',
+      '    X',
+      'XXXXX']
 
-T = ['XXXXX',
-     '  X  ',
-     '  X  ',
-     '  X  ',
-     '  X  ']
+_T = ['XXXXX',
+      '  X  ',
+      '  X  ',
+      '  X  ',
+      '  X  ']
 
-U = ['X   X',
-     'X   X',
-     'X   X',
-     'X   X',
-     'XXXXX']
+_U = ['X   X',
+      'X   X',
+      'X   X',
+      'X   X',
+      'XXXXX']
 
-V = ['X   X',
-     'X   X',
-     'X   X',
-     ' X X ',
-     '  X  ']
+_V = ['X   X',
+      'X   X',
+      'X   X',
+      ' X X ',
+      '  X  ']
 
-W = ['X   X',
-     'X   X',
-     'X X X',
-     'X X X',
-     ' X X ',]
+_W = ['X   X',
+      'X   X',
+      'X X X',
+      'X X X',
+      ' X X ',]
 
-X = ['X   X',
-     ' X X ',
-     '  X  ',
-     ' X X ',
-     'X   X']
+_X = ['X   X',
+      ' X X ',
+      '  X  ',
+      ' X X ',
+      'X   X']
 
-Y = ['X   X',
-     ' X X ',
-     '  X  ',
-     '  X  ',
-     '  X  ']
+_Y = ['X   X',
+      ' X X ',
+      '  X  ',
+      '  X  ',
+      '  X  ']
 
-Z = ['XXXXX',
-     '   X ',
-     '  X  ',
-     ' X   ',
-     'XXXXX']
+_Z = ['XXXXX',
+      '   X ',
+      '  X  ',
+      ' X   ',
+      'XXXXX']
 
-SPACE = ['     ']*5
+_SPACE = ['     ']*5
 
-COMMA = ['     ',
-         '     ',
-         '     ',
-         '   X ',
-         ' XX  ']
+_COMMA = ['     ',
+          '     ',
+          '     ',
+          '   X ',
+          ' XX  ']
 
-DOT = ['     ',
-       '     ',
-       '     ',
-       '     ',
-       '  X  ']
+_DOT = ['     ',
+        '     ',
+        '     ',
+        '     ',
+        '  X  ']
 
-EXCLAMATION = ['  X  ',
-               '  X  ',
-               '  X  ',
-               '     ',
-               '  X  ']
+_EXCLAMATION = ['  X  ',
+                '  X  ',
+                '  X  ',
+                '     ',
+                '  X  ']
 
-CHARS = {'A': A, 'B': B, 'C': C, 'D': D, 'E': E, 'F': F, 'G': G, 'H': H,
-         'I': I, 'J': J, 'K': K, 'L': L, 'M': M, 'N': N, 'O': O, 'P': P,
-         'Q': Q, 'R': R, 'S': S, 'T': T, 'U': U, 'V': V, 'W': W, 'X': X,
-         'Y': Y, 'Z': Z, ' ': SPACE, '!': EXCLAMATION, ',': COMMA,
-         '.': DOT}
+CHARS = {'A': _A, 'B': _B, 'C': _C, 'D': _D, 'E': _E, 'F': _F, 'G': _G, 'H': _H,
+         'I': _I, 'J': _J, 'K': _K, 'L': _L, 'M': _M, 'N': _N, 'O': _O, 'P': _P,
+         'Q': _Q, 'R': _R, 'S': _S, 'T': _T, 'U': _U, 'V': _V, 'W': _W, 'X': _X,
+         'Y': _Y, 'Z': _Z, ' ': _SPACE, '!': _EXCLAMATION, ',': _COMMA,
+         '.': _DOT}
