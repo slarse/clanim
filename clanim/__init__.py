09476bf0cd388608ff21b48e72831cf6264b5237
Simon Larsen <slarse@kth.se>
2017-09-18 17:42:45 +0200

    Add new package level imports

--- clanim/__init__.py
+++ clanim/__init__.py
@@ -1,1 +1,3 @@
-from .decorator import Animate as animate
+from .decorator import Animate as animate, Annotate as annotate
+from .animation.singleline import arrow, char_wave, spinner
+from .animation.multiline import arrows, char_waves, spinners, scrolling_text
